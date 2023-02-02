71199aa4a4edd9f07a56490568a09cca2c8d9da5
Pablo Tilotta <ptilotta@aper.com>
2023-02-01 21:09:56 -0300

    ultimo commit

--- tools/tools.go
+++ tools/tools.go
@@ -35,7 +35,7 @@ func ValidoJWT(t string) (bool, error, string) {
 	var mySecret = "8Xi/PEzDz4P6m9cRMLGZ7ilcxBHIdZfnEgEpw/q4IwA="
 
 	token, err := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
 
