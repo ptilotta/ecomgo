fd714c818ca3e346f4dabb6ea026d7bbfe743067
Pablo Tilotta <ptilotta@aper.com>
2023-03-14 20:18:37 -0300

    ultimo commit

--- main.go
+++ main.go
@@ -40,9 +40,7 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayV2HTTPRequest) 
 	bd.ReadSecret()
 
 	status, message := handlers.Manejadores(path, method, body, headers, request)
-	mensaje, _ := json.Marshal(&Respuesta{
-		message,
-	})
+	mensaje, _ := json.Marshal(message)
 
 	res = &events.APIGatewayProxyResponse{
 		StatusCode: status,
