5430e46f28dd5d7339f1a39e2545f17c2f64f863
Pablo Tilotta <ptilotta@aper.com>
2023-03-28 19:40:43 -0300

    ultimo commit

--- main.go
+++ main.go
@@ -29,7 +29,6 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayV2HTTPRequest) 
 	bd.ReadSecret()
 
 	status, message := handlers.Manejadores(path, method, body, headers, request)
-	//mensaje, _ := json.Marshal(message)
 
 	headersResp := map[string]string{
 		"Content-Type": "application/json",
